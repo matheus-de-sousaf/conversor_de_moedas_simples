84902f7f70404956a2f644df3303087cde11c5e7
Matheus de Sousa <matheussfwp@gmail.com>
2023-09-10 17:41:35 -0300

    Commit_02

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,31 @@
 
-
-
 function Conversor() {
    let valor = document.querySelector('#valor').value
    let moedas = document.querySelector('#moedas').value
    let res = document.querySelector('#res')
-   
+   res.value = ``
+
    switch (moedas) {
 
       case 'Dólar':
-         res.value = res.innerHTML = `${valor * 4.98}`
+         res.value = res.innerHTML = `${valor / 4.98}`
+         break;
       case 'Euro':
-         res.value = res.innerHTML = `${valor * 5.36}`
+         res.value = res.innerHTML = `${valor / 5.36}`
+         break;
       case 'Libra':
-         res.value = res.innerHTML = `${valor * 6.27}`
+         res.value = res.innerHTML = `${valor / 6.27}`
+         break;
       case 'Iene':
-         res.value = res.innerHTML = `${valor * 0.034}`
+         res.value = res.innerHTML = `${valor / 0.034}`
          break;
    }
-
-   console.log(res)
-   console.log(moedas)
 }
-
 
 function altPlace(){
    let res = document.querySelector("#res")
    let moedas = document.querySelector('#moedas').value
    res.setAttribute("placeholder", `${moedas}`);
-   console.log(moedas)
+
+   res.value = res.innerHTML = ``
 }
